f50bbe26f7da9df8c5034369ec244300070b53be
Adrian Ren <adrian.ren@outlook.com>
2018-12-28 22:35:23 +1300

    working on several components

--- src/features/event/EventList/EventListAttendee.js
+++ src/features/event/EventList/EventListAttendee.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import { Image, List } from 'semantic-ui-react';
 
 class EventListAttendee extends Component {
   render() {
+    const {attendee} = this.props;
     return (
-      <div>
-        <h1>EventListAttendee</h1>
-      </div>
+      <List.Item>
+        <Image as={'a'} size={`mini`} circular src={attendee.photoURL}/>
+      </List.Item>
     );
   }
 }
