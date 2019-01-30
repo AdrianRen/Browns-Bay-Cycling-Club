eecb1cec5359220e95956ada92fc69a44b52bc16
Adrian Ren <adrian.ren@outlook.com>
2019-01-30 22:38:52 +1300

    Add event detailed components

--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
 import {Segment, Item, List, Icon, Button} from 'semantic-ui-react';
 import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
   render() {
-    const {event, onEventOpen, deleteEvent} = this.props;
+    const {event, deleteEvent} = this.props;
     return (
       <Segment.Group>
         <Segment>
@@ -36,7 +37,8 @@ class EventListItem extends Component {
         </Segment>
         <Segment clearing>
           <span>{event.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" onClick={onEventOpen(event)}/>
+          {/*<Button as="a" color="teal" floated="right" content="View" onClick={onEventOpen(event)}/>*/}
+          <Button as={Link} to={`/event/${event.id}`} color="teal" floated="right" content="View" />
           <Button as="a" color="red" floated="right" content="Delete" onClick={deleteEvent(event.id)}/>
         </Segment>
       </Segment.Group>
